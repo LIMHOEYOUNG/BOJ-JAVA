8e6fab64caee7d3e84fd06924bb031ac18e7bc08
LIMHOEYEOUNG <dlaghldud@tukorea.ac.kr>
2024-07-05 23:58:11 +0900

    Desktop Code

---
+++ src/Beakjoon29736.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Beakjoon29736 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st= new StringTokenizer(br.readLine());
+        int a= Integer.parseInt(st.nextToken());
+        int b= Integer.parseInt(st.nextToken());
+
+        st= new StringTokenizer(br.readLine());
+        int k= Integer.parseInt(st.nextToken());
+        int x= Integer.parseInt(st.nextToken());
+
+        int ans=0;
+        for(int i=k-x;i<=){
+            if() ans++;
+        }
+
+        System.out.println(ans==0?"IMPOSSIBLE":ans);
+    }
+}
