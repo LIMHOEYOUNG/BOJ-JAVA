5a143994cfb3afb81ad6ac3583256e8dd8b98f82
HoeYoung Lim <dlaghldud@tukorea.ac.kr>
2024-07-20 23:17:16 +0900

    Desktop Code

---
+++ src/Beakjoon26772.java
@@ -0,0 +1,59 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Beakjoon26772 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
+
+        int n= Integer.parseInt(br.readLine());
+
+        StringBuilder sb= new StringBuilder();
+        for(int i=0;i<n;i++){
+            sb.append(" @@@   @@@  ");
+        }
+        sb.append("\n");
+
+        for(int i=0;i<n;i++){
+            sb.append("@   @ @   @ ");
+        }
+        sb.append("\n");
+
+        for(int i=0;i<n;i++){
+            sb.append("@    @    @ ");
+        }
+        sb.append("\n");
+
+        for(int i=0;i<n;i++){
+            sb.append("@         @ ");
+        }
+        sb.append("\n");
+
+        for(int i=0;i<n;i++){
+            sb.append(" @       @  ");
+        }
+        sb.append("\n");
+
+        for(int i=0;i<n;i++){
+            sb.append("  @     @   ");
+        }
+        sb.append("\n");
+
+        for(int i=0;i<n;i++){
+            sb.append("   @   @    ");
+        }
+        sb.append("\n");
+
+        for(int i=0;i<n;i++){
+            sb.append("    @ @     ");
+        }
+        sb.append("\n");
+
+        for(int i=0;i<n;i++){
+            sb.append("     @      ");
+        }
+        sb.append("\n");
+
+        System.out.println(sb);
+    }
+}
