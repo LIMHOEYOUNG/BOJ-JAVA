68c293d4f415fa86254c82cdccb287a95fe730d6
HoeYoung Lim <dlaghldud@tukorea.ac.kr>
2024-07-27 20:49:32 +0900

    Desktop Code

--- src/Beakjoon24087.java
+++ src/Beakjoon24087.java
@@ -6,11 +6,12 @@ public class Beakjoon24087 {
     public static void main(String[] args) throws IOException {
         BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
 
+        int ans=250;
+
         int s= Integer.parseInt(br.readLine());
         int a= Integer.parseInt(br.readLine());
         int b= Integer.parseInt(br.readLine());
 
-        int ans=250;
         for(;a<s;a+=b){
             ans+=100;
         }
