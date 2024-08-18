1361b463fa3547782f2a77d038042a6a52f83d9a
HoeYoung Lim <dlaghldud@tukorea.ac.kr>
2024-08-18 23:17:17 +0900

    20240818

--- src/Beakjoon25858.java
+++ src/Beakjoon25858.java
@@ -1,15 +1,29 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Beakjoon25858 {
     public static void main(String[] args) throws IOException {
         BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
 
-        int a= Integer.parseInt(br.readLine());
-        int b= Integer.parseInt(br.readLine());
-        int c= Integer.parseInt(br.readLine());
+        StringTokenizer st= new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int d = Integer.parseInt(st.nextToken());
 
-        System.out.println(a*b+c);
+        StringBuilder sb= new StringBuilder();
+
+        int num[]= new int[n];
+        int total=0;
+        for(int i=0;i<n;i++){
+            num[i]=Integer.parseInt(br.readLine());
+            total+=num[i];
+        }
+
+        for(int i=0;i<n;i++){
+            sb.append((d*num[i])/total+"\n");
+        }
+
+        System.out.println(sb);
     }
 }
