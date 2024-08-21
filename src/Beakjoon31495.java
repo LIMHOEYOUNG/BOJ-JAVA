ffb127fb486d5152e183234c655e69097f958a3b
HoeYoung Lim <dlaghldud@tukorea.ac.kr>
2024-08-21 23:45:04 +0900

    20240821

---
+++ src/Beakjoon31495.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Beakjoon31495 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
+
+        String str= br.readLine();
+
+        //System.out.println((str.substring(0,1).equals("\"")));
+        //System.out.println((str.substring(str.length()-1,str.length()).equals("\"")));
+        //System.out.println(str.substring(0,1) +" "+str.substring(str.length()-1,str.length()));
+        if((str.length()<=2) || !((str.substring(0,1).equals("\""))&&(str.substring(str.length()-1,str.length()).equals("\"")))){
+            System.out.println("CE");
+        }
+        else{
+            System.out.println(str.substring(1,str.length()-1));
+        }
+    }
+}
