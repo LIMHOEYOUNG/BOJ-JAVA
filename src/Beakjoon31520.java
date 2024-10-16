2980e197008cc7b7169d768b6498c864b1ba0463
HoeYoung Lim <dlaghldud@tukorea.ac.kr>
2024-10-16 23:58:02 +0900

    20241016

--- src/Beakjoon31520.java
+++ src/Beakjoon31520.java
@@ -1,2 +1,20 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 public class Beakjoon31520 {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
+
+        String num= br.readLine();
+        int check= 1;
+
+        for(char c:num.toCharArray()){
+            if(c-'0'!=check++){
+                System.out.println(-1);
+                return;
+            }
+        }
+
+        System.out.println(check-1);
+    }
 }
