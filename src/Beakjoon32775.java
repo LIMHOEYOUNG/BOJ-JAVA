176357164b9abc2062c48cfe3201d83beb320a93
HoeYoung Lim <dlaghldud@tukorea.ac.kr>
2024-12-20 23:56:01 +0900

    20241220

---
+++ src/Beakjoon32775.java
@@ -0,0 +1,14 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Beakjoon32775 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
+
+        int s= Integer.parseInt(br.readLine());
+        int f= Integer.parseInt(br.readLine());
+
+        System.out.println(s>f?"flight":"high speed rail");
+    }
+}
