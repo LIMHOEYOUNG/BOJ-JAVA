9f7d203cca0f0990a6cffca489ba5df01e9baf1f
HoeYoung Lim <dlaghldud@tukorea.ac.kr>
2025-01-07 21:48:36 +0900

    20250107

---
+++ src/Beakjoon31798.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Beakjoon31798 {
+    public static void main(String[] args)throws IOException{
+        BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st= new StringTokenizer(br.readLine());
+        int a= Integer.parseInt(st.nextToken());
+        int b= Integer.parseInt(st.nextToken());
+        int c= Integer.parseInt(st.nextToken());
+
+        if(c==0){
+            System.out.println((int)Math.sqrt(a+b));
+        }
+        else{
+            System.out.println((int)(Math.pow(c,2)-Math.max(a,b)));
+        }
+    }
+}
