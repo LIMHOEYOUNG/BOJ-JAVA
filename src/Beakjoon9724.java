b6f4f57262751d4a65c53e5a40d2acb8c5eeba91
HoeYoung Lim <dlaghldud@tukorea.ac.kr>
2025-01-28 23:17:07 +0900

    20250128

--- src/Beakjoon9724.java
+++ src/Beakjoon9724.java
@@ -1,11 +1,9 @@
-import javax.swing.plaf.synth.SynthTableHeaderUI;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.math.BigInteger;
 import java.util.StringTokenizer;
 
-public class Main {
+public class Beakjoon9724 {
     public static void main(String[] args) throws IOException {
         BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
 
