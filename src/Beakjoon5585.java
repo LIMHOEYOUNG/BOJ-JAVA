526c4813d12b781d7281245a71314902d2037557
HoeYoung Lim <dlaghldud@tukorea.ac.kr>
2025-02-16 21:05:25 +0900

    20250216

--- src/Beakjoon5585.java
+++ src/Beakjoon5585.java
@@ -1,13 +1,18 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
-public class Main {
+public class Beakjoon5585 {
     public static void main(String[] args)throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int n= 1000-Integer.parseInt(br.readLine());
         int ans= 0;
 
+        /*
+        int coins[]= {500,100,50,10,5,1};
+        for(int i=0;i<coins.length;i++){ans+=(n/coins[i]); n%=coins[i];}
+         */
         if(n/500!=0){
             ans+=(n/500);
             n%=500;
@@ -33,17 +38,5 @@ public class Main {
         }
 
         System.out.print(ans);
-
     }
 }
-/*
-501
-
-990
-500 -> 1
-100 -> 4
-50 -> 1
-10 -> 4
-5 -> 1550
-1 -> 4
- */
