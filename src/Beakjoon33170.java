fe10ae6ae04a76ddc80fe7107ae53c186d5a68ca
HoeYoung Lim <dlaghldud@tukorea.ac.kr>
2025-02-21 23:54:44 +0900

    20250221

--- src/Beakjoon33170.java
+++ src/Beakjoon33170.java
@@ -1,8 +1,8 @@
-import javax.swing.plaf.InsetsUIResource;
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
-public class Main {
+public class Beakjoon33170 {
     public static void main(String[] args)throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
